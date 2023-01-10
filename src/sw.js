db4c9348687d0f30fbf7ec17c3064173c9580b34
marmooo <marmooo@users.noreply.github.com>
2023-01-10 12:32:15 +0900

    add js link

--- src/sw.js
+++ src/sw.js
@@ -5,6 +5,7 @@ var urlsToCache = [
   "/waterfall-piano/abt.mid",
   "/waterfall-piano/favicon/favicon.svg",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/combine/npm/tone@14.7.77,npm/@magenta/music@1.23.1/es6/core.js",
 ];
 
 self.addEventListener("install", function (event) {
